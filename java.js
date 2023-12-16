490cc7b85f2514efe92ebd72b4195cd21e3c357f
varun99015 <153973261+varun99015@users.noreply.github.com>
2023-12-16 11:02:14 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+function sound(){
+    var snd = new Audio('high tech.mp3')
+    snd.play()
+}
+ function dsound(){
+    var snd = new Audio('sci-fi.mp3')
+    snd.play()
+ }
+                        
